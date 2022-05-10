6e0a495ddb7d8354f650181957450775e34e68f4
carter <carter.guthrie@gmail.com>
2022-05-09 19:55:17 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ function computerPlay() {
     let randomNum = Math.floor(Math.random() * moveOptions.length);
     return moveOptions[randomNum];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "HA HA IT'S A TIE.";
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "HA HA YOU LOSE.";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "HA HA YOU WIN!";
+    }
+}
+
+const playerSelection = 'Rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
