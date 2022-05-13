50706a0d7548e122208fc7a533ebfe36faa7caa5
carter <carter.guthrie@gmail.com>
2022-05-12 19:50:41 -0500

    Finish else   if

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 function computerPlay() {
-    let moveOptions = ['Rock', 'Paper', 'Scissors'];
+    let moveOptions = ['rock', 'paper', 'scissors'];
     let randomNum = Math.floor(Math.random() * moveOptions.length);
     return moveOptions[randomNum];
 }
 
+playerResponse = prompt("Rock, paper, or scissors?");
+let playerSelection = playerResponse.toLowerCase();
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "HA HA IT'S A TIE.";
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
         return "HA HA YOU LOSE.";
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
         return "HA HA YOU WIN!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "HAHA YOU LOSE!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "HAHA YOU WIN!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "HAHA YOU LOSE!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "HAHA YOU WIN!";
     }
+
 }
 
-const playerSelection = 'Rock';
+
+
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
-
